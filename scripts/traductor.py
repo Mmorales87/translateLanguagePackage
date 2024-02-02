3972e5a7ece8ab8b63bcd6e9829a387a9951a6f3
Mmorales87 <marc.morales@tresipunt.com>
2024-02-02 16:12:42 +0100

    feature: the softcatala API is used to correctly translate into Catalan

--- scripts/traductor.py
+++ scripts/traductor.py
@@ -1,7 +1,47 @@
 import re
-from googletrans import Translator
 import os
-import json
+import requests
+from googletrans import Translator
+
+SPANISH = "es"
+ENGLISH = "en"
+FRENCH = "fr"
+GERMAN = "de"
+ITALIAN = "it"
+PORTUGUESE = "pt"
+CATALAN = "ca"
+DUTCH = "nl"
+SWEDISH = "sv"
+NORWEGIAN = "no"
+DANISH = "da"
+FINNISH = "fi"
+RUSSIAN = "ru"
+CHINESE = "zh"
+JAPANESE = "ja"
+KOREAN = "ko"
+ARABIC = "ar"
+HEBREW = "he"
+HINDI = "hi"
+TURKISH = "tr"
+
+
+def translate_to_catalan(text):
+    try:
+        response = requests.get(
+            "https://www.softcatala.org/api/traductor/translate",
+            params={
+                "langpair": f"spa|cat",  # Establece el par de idiomas de español a catalán
+                "q": text.strip(),
+            }
+        )
+        response.raise_for_status()
+        translated_text = response.json()["responseData"]["translatedText"]
+    except Exception as e:
+        print(f"Error during translation: {e}")
+        translated_text = text.strip()
+
+    return translated_text
+
 
 def file_translate(input_file, output_file, final_language):
     translator = Translator()
@@ -15,12 +55,14 @@ def file_translate(input_file, output_file, final_language):
         match = re.match(r"(\$string\['[^']+'] = ')([^']*)';", line)
         if match:
             prefix, value = match.groups()
-            try:
-                text_translate = translator.translate(value.strip(), dest=final_language).text
-            except Exception as e:
-                print(f"Error during translation: {e}")
-                text_translate = value.strip()
-            translate = f"{prefix}{text_translate}';\n"
+
+            if final_language.lower() == CATALAN:
+                translated_text = translate_to_catalan(value)
+            else:
+                # Utilizar googletrans para traducir a otros idiomas
+                translated_text = translator.translate(value.strip(), dest=final_language).text
+
+            translate = f"{prefix}{translated_text}';\n"
             translations.append(translate)
             print(f"Translated: {line.strip()} -> {translate.strip()}")
         else:
@@ -29,13 +71,14 @@ def file_translate(input_file, output_file, final_language):
     with open(output_file, 'w', encoding='utf-8') as file:
         file.writelines(translations)
 
-if __name__ == "__main__":
-    input_txt = os.path.join(os.path.dirname(__file__), "str-eng.txt")
-    output_txt = "str-cat.txt"
 
-    # idioma al que se quiere traducir
-    target_language = "ca"
+if __name__ == "__main__":
+    input_txt = os.path.join(os.path.dirname(__file__), "workplace-esp.txt")
+    output_txt = "workplace-cat.txt"
+
+    # Idioma al que se quiere traducir
+    target_language = CATALAN
 
     print("Translating...")
     file_translate(input_txt, output_txt, target_language)
-    print("Translation complete.")
+    print(" ------- Translation complete ------- ")
